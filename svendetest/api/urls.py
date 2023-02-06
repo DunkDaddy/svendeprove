a74f33569876598caa893ec5f8208aee95f26cdf
DunkDaddy <59820523+DunkDaddy@users.noreply.github.com>
2023-02-06 12:05:41 +0100

    complete api

--- svendetest/api/urls.py
+++ svendetest/api/urls.py
@@ -7,21 +7,56 @@ urlpatterns = [
     #se alle data
     path('postnummerliste/', postNummer_liste),
     path('personlsite/', person_liste),
+    path('handlingsliste/', handlinger_liste),
+    path('hjliste/', hj_liste),
+    path('rapportliste/', rapport_liste),
+    path('rjliste/', rj_liste),
+    path('pgliste/', pointGrade_liste),
+    path('permissionsliste/', permissions_liste),
+    path('upliste/', up_liste),
 
     #opret data
     path('postnummercreate/', postNummer_create),
     path('personcreate/', person_create),
+    path('handlingercreate/', handlinger_create),
+    path('hjcreate/', hj_create),
+    path('rapportcreate/', rapport_create),
+    path('rjcreate/', rj_create),
+    path('pgcreate/', pointGrade_create),
+    path('permissionscreate/', permissions_create),
+    path('upcreate/',up_create),
 
     #se specifik data
     path('postnr/<int:pk>/', postNummer_view),
     path('person/<int:pk>/', person_view),
+    path('handling/<int:pk>/', handlinger_view),
+    path('hj/<int:pk>/', hj_view),
+    path('rapport/<int:pk>/', rapport_view),
+    path('rj/<int:pk>/', rj_view),
+    path('pg/<int:pk>/', pointGrade_view),
+    path('permissions/<int:pk>', permissions_view),
+    path('up/<int:pk>/', up_view),
 
     #updater specifik data
     path('postnummerupdate/<int:pk>/', postNummer_update),
     path('personupdate/<int:pk>/', person_update),
+    path('handlingerupdate/<int:pk>/', handlinger_update),
+    path('hjupdate/<int:pk>/', hj_update),
+    path('rapportupdate/<int:pk>/', rapport_update),
+    path('rjupdate/<int:pk>/', rj_update),
+    path('pgupdate/<int:pk>/', pointGrade_update),
+    path('permissionsupdate/<int:pk>/', permissions_update),
+    path('upupdate/<int:pk>/', up_update),
 
     #slet specifik data
     path('postnummerdelete/<int:pk>/', postnummer_delete),
-    path('persondelete/<int:pk>/', person_delete)
+    path('persondelete/<int:pk>/', person_delete),
+    path('handlingerdelete/<int:pk>/', handlinger_delete),
+    path('hjdelete/<int:pk>/', hj_delete),
+    path('rapportdelete/<int:pk>', rapport_delete),
+    path('rjdelete/<int:pk>/', rj_delete),
+    path('pgdelete/<int:pk>', pointGrade_delete),
+    path('permissionsdelete/<int:pk>', permissions_delete),
+    path('updelete/<int:pk>/', up_delete),
 
 ]
--- svendetest/api/views.py
+++ svendetest/api/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 
 ############# PostNummer
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 def postNummer_liste(request):
     postnr = PostNummer.objects.all()
     serializer = PostNummerSerializer(postnr, many=True)
@@ -29,7 +29,7 @@ def postNummer_create(request):
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 def postNummer_view(request, pk):
     postnr = PostNummer.objects.get(postNr=pk)
     serializer = PostNummerSerializer(postnr, many=False)
@@ -105,22 +105,22 @@ def person_delete(request, pk):
     person.delete()
 
     return Response('Person er stadig i live "wink"')
-############# PostNummer_Slut
+############# Person_Slut
 
 
-############# Person
+############# Handlinger
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def person_liste(request):
-    person = Person.objects.all()
-    serializer = PersonSerializer(person, many=True)
+def handlinger_liste(request):
+    handling = Handlinger.objects.all()
+    serializer = HandlingerSerializer(handling, many=True)
     return Response(serializer.data)
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def person_create(request):
-    serializer = PersonSerializer(data=request.data)
+def handlinger_create(request):
+    serializer = HandlingerSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
@@ -130,17 +130,17 @@ def person_create(request):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def person_view(request, pk):
-    person = Person.objects.get(id=pk)
-    serializer = PersonSerializer(person, many=False)
+def handlinger_view(request, pk):
+    handlinger = Handlinger.objects.get(id=pk)
+    serializer = HandlingerSerializer(handlinger, many=False)
     return Response(serializer.data)
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def person_update(request, pk):
-    person = Person.objects.get(id=pk)
-    serializer = PersonSerializer(instance=person, data=request.data)
+def handlinger_update(request, pk):
+    handlinger = Handlinger.objects.get(id=pk)
+    serializer = HandlingerSerializer(instance=handlinger, data=request.data)
 
     if serializer.is_valid():
         serializer.save()
@@ -150,9 +150,309 @@ def person_update(request, pk):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def person_delete(request, pk):
-    person = Person.objects.get(id=pk)
-    person.delete()
+def handlinger_delete(request, pk):
+    handlinger = Handlinger.objects.get(id=pk)
+    handlinger.delete()
 
-    return Response('Person er stadig i live "wink"')
-############# PostNummer_Slut
+    return Response('Handlinger has been deleted')
+############# Handlinger_Slut
+
+
+############# Handling_Junctions
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def hj_liste(request):
+    hj = Handling_Junctions.objects.all()
+    serializer = HandlingJunctionsSerializer(hj, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def hj_create(request):
+    serializer = HandlingJunctionsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def hj_view(request, pk):
+    hj = Handling_Junctions.objects.get(id=pk)
+    serializer = HandlingJunctionsSerializer(hj, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def hj_update(request, pk):
+    hj = Handling_Junctions.objects.get(id=pk)
+    serializer = HandlingerSerializer(instance=hj, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def hj_delete(request, pk):
+    hj = Handling_Junctions.objects.get(id=pk)
+    hj.delete()
+
+    return Response('hj has been deleted')
+############# Handling_Junctions_Slut
+
+
+############# Rapport
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def rapport_liste(request):
+    rapport = Rapport.objects.all()
+    serializer = RapportSerializer(rapport, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def rapport_create(request):
+    serializer = RapportSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def rapport_view(request, pk):
+    rapport = Rapport.objects.get(id=pk)
+    serializer = RapportSerializer(rapport, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def rapport_update(request, pk):
+    rapport = Rapport.objects.get(id=pk)
+    serializer = RapportSerializer(instance=rapport, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def rapport_delete(request, pk):
+    rapport = Rapport.objects.get(id=pk)
+    rapport.delete()
+
+    return Response('hj has been deleted')
+############# Rapport_Slut
+
+
+############# Rapport_junctions
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def rj_liste(request):
+    rj = Rapport_junctions.objects.all()
+    serializer = RapporjunctionstSerializer(rj, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def rj_create(request):
+    serializer = RapporjunctionstSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def rj_view(request, pk):
+    rj = Rapport_junctions.objects.get(id=pk)
+    serializer = RapporjunctionstSerializer(rj, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def rj_update(request, pk):
+    rj = Rapport_junctions.objects.get(id=pk)
+    serializer = RapporjunctionstSerializer(instance=rj, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def rj_delete(request, pk):
+    rj = Rapport_junctions.objects.get(id=pk)
+    rj.delete()
+
+    return Response('hj has been deleted')
+############# Rapport_junctions_Slut
+
+
+############# PointGrade
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def pointGrade_liste(request):
+    pg = PointGrade.objects.all()
+    serializer = PointGradeSerializer(pg, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def pointGrade_create(request):
+    serializer = PointGradeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def pointGrade_view(request, pk):
+    pg = PointGrade.objects.get(id=pk)
+    serializer = PointGradeSerializer(pg, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def pointGrade_update(request, pk):
+    pg = PointGrade.objects.get(id=pk)
+    serializer = PointGradeSerializer(instance=pg, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def pointGrade_delete(request, pk):
+    pg = PointGrade.objects.get(id=pk)
+    pg.delete()
+
+    return Response('hj has been deleted')
+############# PointGrade_Slut
+
+
+############# Permissions
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def permissions_liste(request):
+    permissions = Permissions.objects.all()
+    serializer = PermissionsSerializer(permissions, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def permissions_create(request):
+    serializer = PermissionsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def permissions_view(request, pk):
+    permissions = Permissions.objects.get(id=pk)
+    serializer = PermissionsSerializer(permissions, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def permissions_update(request, pk):
+    permissions = Permissions.objects.get(id=pk)
+    serializer = PermissionsSerializer(instance=permissions, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def permissions_delete(request, pk):
+    pg = Permissions.objects.get(id=pk)
+    pg.delete()
+
+    return Response('hj has been deleted')
+############# Permissions_Slut
+
+
+############# Uddel_Permissions
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def up_liste(request):
+    up = Uddel_Permissions.objects.all()
+    serializer = Uddel_PermissionsSerializer(up, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def up_create(request):
+    serializer = Uddel_PermissionsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def up_view(request, pk):
+    up = Uddel_Permissions.objects.get(id=pk)
+    serializer = Uddel_PermissionsSerializer(up, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def up_update(request, pk):
+    up = Uddel_Permissions.objects.get(id=pk)
+    serializer = Uddel_PermissionsSerializer(instance=up, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def up_delete(request, pk):
+    up = Uddel_Permissions.objects.get(id=pk)
+    up.delete()
+
+    return Response('hj has been deleted')
+############# Uddel_Permissions_Slut
