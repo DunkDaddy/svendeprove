2fe1efa97222ba5806563bf1c53166d0d9fc2db1
DunkDaddy <59820523+DunkDaddy@users.noreply.github.com>
2023-02-08 10:10:11 +0100

    Oprettet website app
    Har oprettet website app og forbundet den med resten af django projected.

--- svendetest/svendetest/urls.py
+++ svendetest/svendetest/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("data/", include('api.urls')),
+    path("", include('peterplysside.urls')),
 ]
