f0b467a36b30aca79f223efa9a23f4c886a459a7
DunkDaddy <59820523+DunkDaddy@users.noreply.github.com>
2023-02-22 08:11:35 +0100

    updated website

--- svendetest/peterplysside/urls.py
+++ svendetest/peterplysside/urls.py
@@ -4,8 +4,8 @@ import include
 from .views import *
 
 urlpatterns = [
-    path('1', test, name='home'),
-    path('2', test2, name='home2'),
-    path('3', test3, name='home3'),
+    #path('1', test, name='home'),
+    #path('2', test2, name='home2'),
+    #path('3', test3, name='home3'),
     path('', index, name='index'),
 ]
--- svendetest/peterplysside/views.py
+++ svendetest/peterplysside/views.py
@@ -11,49 +11,68 @@ url1 = 'http://10.130.54.25:8000/data/rjliste/'
 url2 = 'http://10.130.54.25:8000/data/personlsite/'
 headers = {'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/json',
            'Authorization': 'Token 10221976ae7eebb749b62cb74de527cd6500697a'}
+#res = requests.get(url1, headers=headers).json()
+#res2 = requests.get(url2, headers=headers).json()
+#form = ReportForm
 
 
-
-def test(response):
-    res = requests.get(url1, headers=headers).json()
-    form = ReportForm
-    return render(response, "peterplysside/home.html", {"form": form, "res": res})
-
-
-def test2(response):
-    res = requests.get(url1, headers=headers).json()
-    res2 = requests.get(url2, headers=headers).json()
-    form = ReportForm
-    return render(response, "peterplysside/home2.html", {"form": form, "response": res2, "res": res})
+#def test(response):
+#    res = requests.get(url1, headers=headers).json()
+#    form = ReportForm
+#    return render(response, "peterplysside/home.html", {"form": form, "res": res})
 
 
-def test3(response):
-    res = requests.get(url1, headers=headers).json()
-    res2 = requests.get(url2, headers=headers).json()
-    form = ReportForm
-    return render(response, "peterplysside/home3.html", {"form": form, "response": res2, "res": res})
+#def test2(response):
+#    res = requests.get(url1, headers=headers).json()
+#    res2 = requests.get(url2, headers=headers).json()
+#    form = ReportForm
+#    return render(response, "peterplysside/home2.html", {"form": form, "response": res2, "res": res})
+
+
+#def test3(response):
+#    res = requests.get(url1, headers=headers).json()
+#    res2 = requests.get(url2, headers=headers).json()
+#    form = ReportForm
+#    return render(response, "peterplysside/home3.html", {"form": form, "response": res2, "res": res})
 
 
 def index(response):
     if response.method == "POST":
+        check = True
         signup = signupForm(response.POST)
         login = loginForm(response.POST)
+        personlist = Person.objects.all()
         if signup.is_valid():
-            s = Person(navn=signup.cleaned_data["navn"], adresse=signup.cleaned_data["adresse"], mail=signup.cleaned_data["mail"], tlf=signup.cleaned_data["tlf"], postnummer=signup.cleaned_data["postnummer"], cpr=signup.cleaned_data["cpr"], brugernavn=signup.cleaned_data["brugernavn"], password=signup.cleaned_data["password"])
-            s.save()
-            res = requests.get(url1, headers=headers).json()
-            form = ReportForm
-            x = s.point / 3 - 1
-            return render(response, "peterplysside/home.html", {"form": form, "res": res, "person": s, "purple": x})
+            for person in personlist:
+                if signup.cleaned_data['brugernavn'] == person.brugernavn and signup.cleaned_data['password']:
+                    signup = signupForm
+                    login = loginForm
+                    return render(response, "peterplysside/index.html", {"signup": signup, "login": login})
+                else:
+                    s = Person(navn=signup.cleaned_data["navn"], adresse=signup.cleaned_data["adresse"], mail=signup.cleaned_data["mail"], tlf=signup.cleaned_data["tlf"], postnummer=signup.cleaned_data["postnummer"], cpr=signup.cleaned_data["cpr"], brugernavn=signup.cleaned_data["brugernavn"], password=signup.cleaned_data["password"])
+                    for person in personlist:
+                        if s.brugernavn == person.brugernavn and s.password == person.password:
+                            check = False
+                    if check == True:
+                        s.save()
+                    res = requests.get(url1, headers=headers).json()
+                    form = ReportForm
+                    x = 233 - (s.point / 3 - 1)
+                    return render(response, "peterplysside/home.html", {"form": form, "res": res, "person": s, "purple": x})
+
         if login.is_valid():
-            personlist = Person.objects.all()
             for person in personlist:
                 if login.cleaned_data['brugernavn'] == person.brugernavn and login.cleaned_data['password']:
                     s = person
                     res = requests.get(url1, headers=headers).json()
                     form = ReportForm
-                    x = s.point / 3 - 1
-                    return render(response, "peterplysside/home.html", {"form": form, "res": res, "person": s, "purple": x})
+                    x = 233 - (s.point / 3 - 1)
+                    if s.point > 500:
+                        res2 = requests.get(url2, headers=headers).json()
+                        return render(response, "peterplysside/home2.html",{"form": form, "res": res, "person": s, "people": res2, "purple": x})
+                    else:
+                        return render(response, "peterplysside/home.html", {"form": form, "res": res, "person": s, "purple": x})
+
     else:
         signup = signupForm
         login = loginForm
